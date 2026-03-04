6a8ba7eb6aebe80057c56ac6455fd5b2be11aedf
zduv <116897754+zduvv@users.noreply.github.com>
2026-03-04 20:38:38 +0100

    Add files via upload

--- tiktok.py
+++ tiktok.py
@@ -1,35 +1,13 @@
-import aiohttp
-
-from typing import Any
-from pydantic import BaseModel
-from discord.ext import commands
-from tools.helpers import PretendContext
+from colorama import Fore, init
 
 
-class CashApp(BaseModel):
-    """
-    Model for cashapp user
-    """
-
-    url: str
-    qr: Any
-
-
-class CashappUser(commands.Converter):
-    async def convert(self, ctx: PretendContext, argument: str) -> CashApp:
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get(f"https://cash.app/${argument}") as r:
-                if r.status == 404:
-                    raise commands.BadArgument("Cashapp profile not found")
-                if r.status == 200:
-                    data = {
-                        "url": str(r.url),
-                        "qr": await ctx.bot.getbyte(
-                            f"https://cash.app/qr/${argument}?size=288&margin=0"
-                        ),
-                    }
-                    return CashApp(**data)
-                else:
-                    raise commands.BadArgument(
-                        "There was a problem getting the user's cashapp profile"
-                    )
+class Debug:
+    def __init__(self, enabled):
+        self.enabled = enabled
+        init(True)
+    def success(self, text):
+        if self.enabled:
+            print(f"[{Fore.GREEN}✓{Fore.RESET}] {text}")
+    def error(self, text):
+        if self.enabled:
+            print(f"[{Fore.RED}✗{Fore.RESET}] {text}")
